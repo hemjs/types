94b043f883e41c39daa6e63a755116a554564c9b
augusito <kaugustus15@gmail.com>
2023-01-11 19:08:22 +0300

    refactor: refine types for request methods

--- packages/http-adapter/src/http-adapter.interface.ts
+++ packages/http-adapter/src/http-adapter.interface.ts
@@ -1,35 +1,43 @@
-export type ErrorHandler = <TRequest, TResponse>(
+export type PathArgument = string | RegExp | (string | RegExp)[];
+
+export type NextFunction = (err?: any) => void;
+
+export type ErrorHandler = <TRequest = any, TResponse = any>(
   error: any,
   req: TRequest,
   res: TResponse,
-  next?: Function,
+  next?: NextFunction,
 ) => any;
 
-export type RequestHandler = <TRequest, TResponse>(
+export type RequestHandler = <TRequest = any, TResponse = any>(
   req: TRequest,
   res: TResponse,
-  next?: Function,
+  next?: NextFunction,
 ) => any;
 
+export type Handler = RequestHandler | ErrorHandler;
+
+export type HandlerArgument = Handler | Handler[];
+
 export interface HttpAdapter {
-  use(handler: RequestHandler | ErrorHandler): any;
-  use(path: string, handler: RequestHandler | ErrorHandler): any;
-  get(handler: RequestHandler): any;
-  get(path: string, handler: RequestHandler | RequestHandler[]): any;
-  post(handler: RequestHandler | RequestHandler[]): any;
-  post(path: string, handler: RequestHandler | RequestHandler[]): any;
-  head(handler: RequestHandler | RequestHandler[]): any;
-  head(path: string, handler: RequestHandler | RequestHandler[]): any;
-  delete(handler: RequestHandler | RequestHandler[]): any;
-  delete(path: string, handler: RequestHandler | RequestHandler[]): any;
-  put(handler: RequestHandler | RequestHandler[]): any;
-  put(path: string, handler: RequestHandler | RequestHandler[]): any;
-  patch(handler: RequestHandler | RequestHandler[]): any;
-  patch(path: string, handler: RequestHandler | RequestHandler[]): any;
-  all(path: string, handler: RequestHandler | RequestHandler[]): any;
-  all(handler: RequestHandler | RequestHandler[]): any;
-  options(handler: RequestHandler | RequestHandler[]): any;
-  options(path: string, handler: RequestHandler | RequestHandler[]): any;
+  use(handler: HandlerArgument[]): any;
+  use(mountPoint: PathArgument, handler: HandlerArgument[]): any;
+  get(handler: HandlerArgument[]): any;
+  get(path: PathArgument, handler: HandlerArgument[]): any;
+  post(handler: HandlerArgument[]): any;
+  post(path: PathArgument, handler: HandlerArgument[]): any;
+  head(handler: HandlerArgument[]): any;
+  head(path: PathArgument, handler: HandlerArgument[]): any;
+  delete(handler: HandlerArgument[]): any;
+  delete(path: PathArgument, handler: HandlerArgument[]): any;
+  put(handler: HandlerArgument[]): any;
+  put(path: PathArgument, handler: HandlerArgument[]): any;
+  patch(handler: HandlerArgument[]): any;
+  patch(path: PathArgument, handler: HandlerArgument[]): any;
+  all(path: PathArgument, handler: HandlerArgument[]): any;
+  all(handler: HandlerArgument[]): any;
+  options(handler: HandlerArgument[]): any;
+  options(path: PathArgument, handler: HandlerArgument[]): any;
   listen(port: number | string, callback?: () => void): any;
   listen(port: number | string, hostname: string, callback?: () => void): any;
   status(response: any, statusCode: number): any;
